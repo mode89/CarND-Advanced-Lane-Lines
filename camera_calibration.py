0a726be9ed25b4269d98285c479b6047a5eec43b
Mode <mode89@mail.ru>
2018-04-25 00:25:44 +0800

    implement CameraModel class

--- camera_calibration.py
+++ camera_calibration.py
@@ -3,73 +3,89 @@ import cv2
 import glob
 import numpy as np
 
-def calibrate_camera(nx, ny):
-    images = list()
-    image_paths = glob.glob("camera_cal/*.jpg")
-    print("Loading calibration images ...")
-    for path in image_paths:
-        images.append(cv2.imread(path))
-    image_size = images[0].shape[1::-1]
+class CameraModel:
 
-    object_points = [(i, j, 0) for i in range(ny) for j in range(nx)]
-    all_object_points = list()
-    all_image_points = list()
+    def calibrate(self, nx, ny):
+        images = list()
+        image_paths = glob.glob("camera_cal/*.jpg")
+        print("Loading calibration images ...")
+        for path in image_paths:
+            images.append(cv2.imread(path))
+        image_size = images[0].shape[1::-1]
 
-    print("Finding chessboard corners ...")
-    for image in images:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(image, (nx, ny))
-        if ret:
-            all_object_points.append(object_points)
-            all_image_points.append(corners)
+        object_points = [(i, j, 0) for i in range(ny) for j in range(nx)]
+        all_object_points = list()
+        all_image_points = list()
 
-    print("Calculating calibration matrix ...")
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-        np.float32(all_object_points),
-        np.float32(all_image_points),
-        image_size,
-        None,
-        None)
+        print("Finding chessboard corners ...")
+        for image in images:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            ret, corners = cv2.findChessboardCorners(image, (nx, ny))
+            if ret:
+                all_object_points.append(object_points)
+                all_image_points.append(corners)
 
-    return mtx, dist
+        print("Calculating calibration matrix ...")
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+            np.float32(all_object_points),
+            np.float32(all_image_points),
+            image_size,
+            None,
+            None)
 
-def save_calibration_data(cameraMatrix, distortionCoeffs):
-    print("Saving calibration data ...")
-    np.savez("calibration_data",
-        cameraMatrix=cameraMatrix,
-        distortionCoeffs=distortionCoeffs)
+        self.cameraMatrix = mtx
+        self.distortionCoeffs = dist
 
-def load_calibration_data():
-    print("Loading calibration data ...")
-    with np.load("calibration_data.npz") as data:
-        return data["cameraMatrix"], data["distortionCoeffs"]
+    def find_perspective_transformation(self):
+        src = np.float32([
+            (560, 475),
+            (725, 475),
+            (298, 660),
+            (1010, 660)
+        ])
 
-def undistort_image(image, cameraMatrix, distortionCoeffs):
-    print("Undistorting image ...")
-    return cv2.undistort(image, cameraMatrix, distortionCoeffs)
+        dst = np.float32([
+            (300, 1000),
+            (670, 1000),
+            (300, 2220),
+            (670, 2220)
+        ])
 
-def perspective_transformation():
-    src = np.float32([
-        (560, 475),
-        (725, 475),
-        (298, 660),
-        (1010, 660)
-    ])
+        self.perspectiveMatrix = cv2.getPerspectiveTransform(src, dst)
+        self.perspectiveMatrixInv = cv2.getPerspectiveTransform(dst, src)
 
-    dst = np.float32([
-        (300, 1000),
-        (670, 1000),
-        (300, 2220),
-        (670, 2220)
-    ])
+    def bird_view(self, image):
+        image = cv2.undistort(
+            image, self.cameraMatrix, self.distortionCoeffs)
+        image = cv2.warpPerspective(
+            image, self.perspectiveMatrix, (970, 2220))
+        return image
 
-    perspMat = cv2.getPerspectiveTransform(src, dst)
-    perspMatInv = cv2.getPerspectiveTransform(dst, src)
+    def save(self):
+        print("Saving calibration data ...")
+        np.savez("camera_model",
+            cameraMatrix=self.cameraMatrix,
+            distortionCoeffs=self.distortionCoeffs,
+            perspectiveMatrix=self.perspectiveMatrix,
+            perspectiveMatrixInv=self.perspectiveMatrixInv)
 
-    return perspMat, perspMatInv
+    def load(self):
+        print("Loading calibration data ...")
+        with np.load("camera_model.npz") as data:
+            self.cameraMatrix = data["cameraMatrix"]
+            self.distortionCoeffs = data["distortionCoeffs"]
+            self.perspectiveMatrix = data["perspectiveMatrix"]
+            self.perspectiveMatrixInv = data["perspectiveMatrixInv"]
 
 if __name__ == "__main__":
-    cameraMatrix, distortionCoeffs = calibrate_camera(9, 6)
+    camera_model = CameraModel()
+    camera_model.calibrate(9, 6)
+    camera_model.find_perspective_transformation()
+    camera_model.save()
+    camera_model.load()
 
     image = cv2.imread("test_images/test1.jpg")
-    image = undistort_image(image, cameraMatrix, distortionCoeffs)
+    image = camera_model.bird_view(image)
+    cv2.namedWindow("image", cv2.WINDOW_KEEPRATIO)
+    cv2.imshow("image", image)
+    cv2.waitKey(0)
